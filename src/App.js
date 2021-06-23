0be374f2b5bd566b5af9714e9ce79255150ec93b
[yagne] <[yagnesh.patel9898@gmail.com]>
2021-06-23 13:42:10 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import {  useState } from "react";
+import City from "./City";
+
+
+// require('dotenv').config('../.env');
 
 function App() {
+
+  const ApiKey = process.env.REACT_APP_API_KEY;
+
+  const [city, setCity] = useState("");
+
+  const [data, setCityWeather] = useState("");
+
+  const [error, setError] = useState();
+  
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${ApiKey}`;
+
+  function changeHandler(e) {
+    e.preventDefault();
+    setCity(e.currentTarget.value);
+  }
+
+  const fetchUrl = async () => {
+    const response = await fetch(url);
+    console.log(response.ok)
+    if (response.ok) {
+      const data = await response.json();
+      const wetherinfo = {
+        city: data.name,
+        country: data.sys.country,
+        wethertype: data.weather[0].main,
+        description: data.weather[0].description,
+        temperature: parseInt(data.main.temp - 273),
+        temp_feelsLike: parseInt(data.main.feels_like - 273),
+        sunrise: data.sys.sunrise,
+        sunset: data.sys.sunset,
+        max_temp: parseInt(data.main.temp_max - 273),
+        min_temp: parseInt(data.main.temp_min - 273),
+        icon: data.weather[0].icon,
+      };
+      setCityWeather(wetherinfo);
+      setError(false)
+    } else {
+      
+      setError(true)
+      setCityWeather("")
+    }
+  };
+
+  function onClickHandler(e) {
+    fetchUrl();
+    
+  }
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+
+
+
+      <div className="search">
+        <input
+          type="text"
+          className="searchTerm"
+          placeholder="Enter City"
+          onChange={(e) => {
+            changeHandler(e);
+          }}
+        />
+        <button
+          type="submit"
+          className="searchButton"
+          onClick={(e) => {
+            onClickHandler(e);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <i>
+            <FontAwesomeIcon icon={faSearch} />
+          </i>
+        </button>
+      </div>
+
+      { data && <City value = {data}  /> } 
+       { error && <p className="error"> Not found!</p>}
     </div>
   );
 }
---
+++ src/City.js
@@ -0,0 +1,27 @@
+import { React } from "react";
+import "./city.css";
+
+const City = ({ value }) => {
+  // console.log(value , "City component")
+  // console.log(value.wethertype)
+
+  const date = new Date();
+
+  let img = "http://openweathermap.org/img/wn/" + value.icon + "@2x.png";
+
+  return (
+    <div className="container">
+      <div className="day">{date.toDateString()}</div>
+
+      <div className="temp">{value.temperature}°C</div>
+
+      <div className="citys">
+        {value.city},{value.country}
+      </div>
+      <img src={img} className="weather-icon" alt={value.icon} />
+      <div className="desc">{value.description}</div>
+    </div>
+  );
+};
+
+export default City;
